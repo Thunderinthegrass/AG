c4a30afb889824bf612c600769cf6360fca46a2a
ThunderInTheGrass <propeller8924@gmail.com>
2022-07-07 12:13:26 +0800

    header

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+function mobileMenu() {
+  let menu = document.querySelector('.mobile-menu');
+  let menuBtn = document.querySelector('.mobile-menu__btn');
+  let btnClose = document.querySelector('.mobile-menu__close-btn');
+
+  menuBtn.addEventListener('click', () => {
+    menu.classList.remove('d-none');
+  })
+
+  btnClose.addEventListener('click', () => {
+    menu.classList.add('d-none');
+  })
+}
+mobileMenu();
