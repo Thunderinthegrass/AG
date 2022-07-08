214a4106910a38da7ce623239f97675948128fba
ThunderInTheGrass <propeller8924@gmail.com>
2022-07-09 00:25:58 +0800

    9

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,24 @@
-// function mobileMenu() {
-//   let menu = document.querySelector('.mobile-menu');
-//   let menuBtn = document.querySelector('.mobile-menu__btn');
-//   let btnClose = document.querySelector('.mobile-menu__close-btn');
+function mobileMenu() {
+  let menu = document.querySelector('.mobile-menu');
+  let menuBtn = document.querySelector('.mobile-menu__btn');
+  let btnClose = document.querySelector('.mobile-menu__close-btn');
 
-//   menuBtn.addEventListener('click', () => {
-//     menu.classList.remove('d-none');
-//   })
+  menuBtn.addEventListener('click', () => {
+    menu.classList.remove('d-none');
+  })
 
-//   btnClose.addEventListener('click', () => {
-//     menu.classList.add('d-none');
-//   })
-// }
-// mobileMenu();
+  btnClose.addEventListener('click', () => {
+    menu.classList.add('d-none');
+  })
+}
+mobileMenu();
 
-// $(function(){
-//   $('.cases__slider').slick({
-//     infinite: true,
-//     fade: true,
-//     dots: true,
-//     prevArrow: '<img class="slider-arrows slider-arrows__left"src="img/slider-arrows/arrow-left.svg" alt="">',
-//     nextArrow: '<img class="slider-arrows slider-arrows__right"src="img/slider-arrows/arrow-right.svg" alt="">',
-//   });
-// });
+$(function(){
+  $('.cases__slider').slick({
+    infinite: true,
+    fade: true,
+    dots: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left"src="img/slider-arrows/arrow-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right"src="img/slider-arrows/arrow-right.svg" alt="">',
+  });
+});
