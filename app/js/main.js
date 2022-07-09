ba8e2a4d0bf55cb043a0178d47633e76b7e9eb53
ThunderInTheGrass <propeller8924@gmail.com>
2022-07-10 00:58:20 +0800

    6

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,17 @@
-function mobileMenu() {
-  let menu = document.querySelector('.mobile-menu');
-  let menuBtn = document.querySelector('.mobile-menu__btn');
-  let btnClose = document.querySelector('.mobile-menu__close-btn');
+// function mobileMenu() {
+//   let menu = document.querySelector('.mobile-menu');
+//   let menuBtn = document.querySelector('.mobile-menu__btn');
+//   let btnClose = document.querySelector('.mobile-menu__close-btn');
 
-  menuBtn.addEventListener('click', () => {
-    menu.classList.remove('d-none');
-  })
+//   menuBtn.addEventListener('click', () => {
+//     menu.classList.remove('d-none');
+//   })
 
-  btnClose.addEventListener('click', () => {
-    menu.classList.add('d-none');
-  })
-}
-mobileMenu();
+//   btnClose.addEventListener('click', () => {
+//     menu.classList.add('d-none');
+//   })
+// }
+// mobileMenu();
 
 $(function(){
   $('.cases__slider').slick({
