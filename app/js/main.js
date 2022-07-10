8b7494183a8b0bf0546850114a28c12486e8c349
ThunderInTheGrass <propeller8924@gmail.com>
2022-07-10 14:50:28 +0800

    7

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,19 @@
 // }
 // mobileMenu();
 
+function viewAll() {
+  const btn = document.querySelector('.view-all');
+  const sliders = document.querySelectorAll('.cases__slider-item');
+
+  btn.addEventListener('click', () => {
+    btn.classList.add('d-none')
+    sliders.forEach((item) => {
+      item.classList.add('d-block');
+    })
+  })
+}
+viewAll();
+
 $(function(){
   $('.cases__slider').slick({
     infinite: true,
@@ -20,5 +33,11 @@ $(function(){
     dots: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left"src="img/slider-arrows/arrow-left.svg" alt="">',
     nextArrow: '<img class="slider-arrows slider-arrows__right"src="img/slider-arrows/arrow-right.svg" alt="">',
+    responsive: [
+      {
+         breakpoint: 660,
+         settings: "unslick"
+      }
+   ]
   });
 });
