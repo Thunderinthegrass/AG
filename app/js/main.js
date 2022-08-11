39781e8c1c24b27f2727c9e3baec4afc6fa64b1b
ThunderInTheGrass <propeller8924@gmail.com>
2022-08-11 20:21:51 +0800

    platforms

--- app/js/main.js
+++ app/js/main.js
@@ -1,40 +1,43 @@
 function mobileMenu() {
-  let menu = document.querySelector('.mobile-menu');
-  let menuBtn = document.querySelector('.mobile-menu__btn');
-  let btnClose = document.querySelector('.mobile-menu__close-btn');
+  let menu = document.querySelector(".mobile-menu");
+  let menuBtn = document.querySelector(".mobile-menu__btn");
+  let btnClose = document.querySelector(".mobile-menu__close-btn");
 
-  menuBtn.addEventListener('click', () => {
-    menu.classList.remove('d-none');
-  })
+  menuBtn.addEventListener("click", () => {
+    menu.classList.remove("d-none");
+  });
 
-  btnClose.addEventListener('click', () => {
-    menu.classList.add('d-none');
-  })
+  btnClose.addEventListener("click", () => {
+    menu.classList.add("d-none");
+  });
 }
 mobileMenu();
 
 function viewAll() {
-  const btn = document.querySelector('.view-all');
-  const sliders = document.querySelectorAll('.cases__slider-item');
+  const btn = document.querySelector(".view-all");
+  const sliders = document.querySelectorAll(".cases__slider-item");
 
-  btn.addEventListener('click', () => {
-    btn.classList.add('d-none')
+  btn.addEventListener("click", () => {
+    btn.classList.add("d-none");
     sliders.forEach((item) => {
-      item.classList.add('d-block');
-    })
-  })
+      item.classList.add("d-block");
+    });
+  });
 }
 viewAll();
 
-const anchors = document.querySelectorAll('a[href*="#"]')
+function scrollToLink() {
+  const anchors = document.querySelectorAll('a[href*="#"]');
 
-for (let anchor of anchors) {
-  anchor.addEventListener("click", function (event) {
-    event.preventDefault();
-    const blockID = anchor.getAttribute('href')
-    document.querySelector('' + blockID).scrollIntoView({
-      behavior: "smooth",
-      block: "start"
-    })
-  })
+  for (let anchor of anchors) {
+    anchor.addEventListener("click", function (event) {
+      event.preventDefault();
+      const blockID = anchor.getAttribute("href");
+      document.querySelector("" + blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
 }
+scrollToLink();
